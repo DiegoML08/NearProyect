b3fca052ef713a7a76f21bbd815f2d2a6395dab9
DiegoDev <DiegoM_08.L@outlook.es>
2025-12-31 15:00:34 -0500

    Primera configuracion del Backend

---
+++ src/main/java/com/near/api/NearApiApplication.java
@@ -0,0 +1,13 @@
+package com.near.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NearApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/near/api/NearApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.near.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
